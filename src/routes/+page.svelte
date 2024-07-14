92f19b083f047add3d3881216da93e5928fbb8b8
Valexr <valexr@gmail.com>
2024-07-14 11:27:10 +0200

    fix base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<script lang="ts" context="module">
+    import { base } from '$app/paths';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<a href="/some">some</a>
+<a href="{base}/some">some</a>
